14168d8d66f37cc38dfad3ab71d00a1d67f301db
reyhaneterzioglu <terzioglureyhane@gmail.com>
2022-11-29 16:32:17 +0100

    Consuming APIs

--- spring-17-REST-ConsumingApis/src/main/java/com/cydeo/Spring17RestConsumingApisApplication.java
+++ spring-17-REST-ConsumingApis/src/main/java/com/cydeo/Spring17RestConsumingApisApplication.java
@@ -1,11 +1,14 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients
 public class Spring17RestConsumingApisApplication {
 
 	public static void main(String[] args) {
@@ -16,5 +19,8 @@ public class Spring17RestConsumingApisApplication {
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
+
+	@Bean
+	public ModelMapper mapper(){return new ModelMapper();}
 
 }
