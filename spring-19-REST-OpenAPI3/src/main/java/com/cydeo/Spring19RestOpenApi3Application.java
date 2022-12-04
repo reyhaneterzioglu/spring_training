8a747e82ca8876112ea8b24e7f80645143043692
reyhaneterzioglu <terzioglureyhane@gmail.com>
2022-12-04 14:56:59 +0100

    REST - Open API

---
+++ spring-19-REST-OpenAPI3/src/main/java/com/cydeo/Spring19RestOpenApi3Application.java
@@ -0,0 +1,37 @@
+package com.cydeo;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+@SpringBootApplication
+public class Spring19RestOpenApi3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring19RestOpenApi3Application.class, args);
+    }
+
+    @Bean
+    public ModelMapper mapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public OpenAPI customOpenApi() {
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Cydeo Application OpenaPI")
+                        .version("v1")
+                        .description("Cydeo applicaiton API documentation"))
+                .servers(List.of(new Server().url("https://dev.cydeo.com").description("Dev Environment")));
+
+    }
+
+}
