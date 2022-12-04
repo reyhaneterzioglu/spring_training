4243a0175bc05932d1bd03c652acb41504595284
reyhaneterzioglu <terzioglureyhane@gmail.com>
2022-12-04 14:56:17 +0100

    REST - Lab

---
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/dto/AddressDTO.java
@@ -0,0 +1,40 @@
+package com.cydeo.dto;
+
+import com.cydeo.enums.AddressType;
+import com.fasterxml.jackson.annotation.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class AddressDTO {
+
+    @JsonIgnore
+    private Long id;
+
+    private String street;
+    private String country;
+    private String state;
+    private String city;
+    private String postalCode;
+
+    private AddressType addressType;
+
+    @JsonBackReference(value = "student-address-reference")
+    private StudentDTO student;
+
+    @JsonBackReference(value = "parent-address-reference")
+    private ParentDTO parent;
+
+    @JsonBackReference(value = "teacher-address-reference")
+    private TeacherDTO teacher;
+
+    private Integer currentTemperature; // weather info, which we will later get from 3rd party API
+
+}
